29a0cbaf820191dbc1248f4ff3a13135139ee2c4
yiiwang <yiwangyolo@hotmail.com>
2020-06-01 18:58:28 -0400

    Initial work on adding user connection

--- Ins/admin.py
+++ Ins/admin.py
@@ -1,20 +1,34 @@
 from django.contrib import admin
-from Ins.models import Post, InsUser, Comment, Like
+from Ins.models import Post, InsUser, Comment, Like, UserConnection
 
 # Register your models here.
-class CommentInLine(admin.StackedInline):
+class CommentInline(admin.StackedInline):
     model = Comment
 
-class LikeInLine(admin.StackedInline):
+class LikeInline(admin.StackedInline):
     model = Like
 
 class PostAdmin(admin.ModelAdmin):
     inlines = [
-        CommentInLine,
-        LikeInLine
+        CommentInline,
+        LikeInline
+    ]
+
+class FollowingInline(admin.StackedInline):
+    model = UserConnection
+    fk_name = 'creator'
+
+class FollowerInline(admin.StackedInline):
+    model = UserConnection
+    fk_name = 'following'
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        FollowingInline,
+        FollowerInline
     ]
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(InsUser)
+admin.site.register(InsUser, UserAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
--- Ins/models.py
+++ Ins/models.py
@@ -13,6 +13,40 @@ class InsUser(AbstractUser):
 		blank = True
 	)
 
+	def get_connection(self):
+		connections = UserConnection.objects.filter(creator = self)
+		return connections
+
+	def get_followers(self):
+		followers = UserConnection.objects.filter(following = self)
+		return followers
+
+	def is_followed_by(self, user):
+		followers = UserConnection.objects.filter(following = self)
+		return followers.filter(creator = user).exists()
+
+	def __str__(self):
+		return self.username
+
+class UserConnection(models.Model):
+	created = models.DateTimeField(
+		auto_now_add = True,
+		editable = False
+	)
+	creator = models.ForeignKey(
+		InsUser,
+		on_delete = models.CASCADE,
+		related_name = 'friendship_creator_set'
+	)
+	following = models.ForeignKey(
+		InsUser,
+		on_delete = models.CASCADE,
+		related_name = 'friend_set'
+	)
+
+	def __str__(self):
+		return self.creator.username + ' follows ' + self.following.username
+
 class Post(models.Model):
 	author = models.ForeignKey(
 		InsUser,
@@ -21,7 +55,10 @@ class Post(models.Model):
 		on_delete = models.CASCADE,
 		related_name = 'ins_posts'
 	)
-	title = models.TextField(blank=True, null=True)
+	title = models.TextField(
+		blank=True, 
+		null=True
+	)
 	image = ProcessedImageField(
 		upload_to = 'static/images/posts',
 		format = 'JPEG',
@@ -39,21 +76,46 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def get_like_count(self):
+		return self.likes.count()
+
+	def get_comment_count(self):
+		return self.comments.count()
+
 	def get_absolute_url(self):
-		return reverse("post", args = [str(self.id)])
+		return reverse('post', args = [str(self.id)])
 
 class Comment(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-	user = models.ForeignKey(InsUser, on_delete=models.CASCADE)
-	comment = models.CharField(max_length=100)
-	posted_on = models.DateTimeField(auto_now_add=True, editable=False)
+	post = models.ForeignKey(
+		Post, 
+		on_delete=models.CASCADE, 
+		related_name='comments'
+	)
+	user = models.ForeignKey(
+		InsUser, 
+		on_delete=models.CASCADE
+	)
+	comment = models.CharField(
+		max_length=100
+	)
+	posted_on = models.DateTimeField(
+		auto_now_add=True, 
+		editable=False
+	)
 
 	def __str__(self):
 		return self.comment
 
 class Like(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-	user = models.ForeignKey(InsUser, on_delete=models.CASCADE)
+	post = models.ForeignKey(
+		Post, 
+		on_delete=models.CASCADE, 
+		related_name='likes'
+	)
+	user = models.ForeignKey(
+		InsUser, 
+		on_delete=models.CASCADE
+	)
 
 	class Meta:
 		unique_together = ('post', 'user')
